c1f31f08621d917c456e8c187676fbdd913b3dae
Cyril Cappendijk <ccappendijk@hotmail.com>
2023-09-27 19:17:29 +0200

    Dev interfuc 27092023 alleen .tlc en .add en projectfiles

--- starvar.h
+++
@@ -1,8 +0,0 @@
-/* starvar.h - gegenereerd met TLCGen 0.12.2.0 */
-
-extern mulv star_programma; 
-extern mulv STAR_ctijd[STARMAX];
-extern mulv STAR_start1[STARMAX][FCMAX];
-extern mulv STAR_eind1[STARMAX][FCMAX];
-extern mulv STAR_start2[STARMAX][FCMAX];
-extern mulv STAR_eind2[STARMAX][FCMAX];
